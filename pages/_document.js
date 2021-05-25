08b5c80efebe12a12f5a79dc8f9629b808da4482
dbeetoven <beetovenson@gmail.com>
2021-05-24 21:01:33 -0300

    adding google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,23 @@ class MyDocument extends Document {
                     <meta property="og:site_name" content={siteName} key="ogsitename" />
                     <meta property="og:title" content={siteTitle} key="ogtitle" />
                     <meta property="og:description" content={description} key="ogdesc" />
+
+                    <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
                 </Head>
                 <body>
                 <Main />
